62710df216eb89123aca05ae74c5973c405b7177
Richard (Rick) Hawkins <richard.hawkins@rackspace.com>
2014-09-29 17:51:59 -0400

    Created middleware module

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[],
     entry_points={
         'paste.filter_factory': [
-            'swift_inspector=swift_inspector.middleware:filter_factory',
+            'swift_proxy_inspector=swift_inspector.middleware.proxy:filter_factory',
             ],
         },
     )
